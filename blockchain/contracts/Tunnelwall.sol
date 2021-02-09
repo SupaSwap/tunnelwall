b9b79afdd6432e70bdab23daf334305bcab929d7
Josh Stow <joshxstow@gmail.com>
2021-02-09 00:11:03 +0000

    add logic to set first item of mapping Wall when contract is deployed

--- blockchain/contracts/Tunnelwall.sol
+++ blockchain/contracts/Tunnelwall.sol
@@ -14,8 +14,16 @@ contract Tunnelwall {
 
     mapping(uint256 => Message) Wall;
     
+    /*
+     * @dev Set first item in Wall mapping when contract is deployed
+     */
     constructor () {
         owner = msg.sender;
+        uid = 1;
+        Wall[uid] = Message(
+            0x5468652074756e6e656c20626567696e7320686572652e000000000000000000,
+            block.timestamp
+        );
     }
 
 }
