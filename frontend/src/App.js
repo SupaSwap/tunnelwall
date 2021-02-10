c9821a8a816c5d093a4639deaaa957f265635ee0
Josh Stow <joshxstow@gmail.com>
2021-02-10 16:31:08 +0000

    import web3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import Web3 from 'web3';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+
+const web3 = new Web3(Web3.givenProvider);  // use the given Provider or instantiate a new websocket provider
 
 function App() {
   return (
