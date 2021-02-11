f5471acb7de9cbddd8f7109a8f3bf9fe43e1c3d7
Josh Stow <joshxstow@gmail.com>
2021-02-11 15:45:22 +0000

    add link to github

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Web3 from 'web3';
 import { tunnelwallAbi } from './abi';
 import { Navbar, Jumbotron, Form, FormGroup, Button, Alert, Container } from 'react-bootstrap';
 import { ReactComponent as Logo } from './logo.svg';
+import { ReactComponent as GithubLogo } from './github.svg';
 import MessageCard from './MessageCard';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
@@ -56,19 +57,34 @@ function App() {
             }}/>{' '}
             Tunnelwall 
           </Navbar.Brand>
-          <span className="navbar-text">
+          <span className="navbar-text mr-1">
           { walletAddress }
           </span>
         </Container>
       </Navbar>
       <Jumbotron style={{
         borderRadius: "0",
-        backgroundImage: "linear-gradient(120deg, #b721ff 0%, #21d4fd 100%)"
+        backgroundImage: "linear-gradient(120deg, #b721ff 0%, #21d4fd 100%)",
+        textAlign: "center"
       }}>
-        <h1 className="text-white text-center mt-5">Welcome to the Tunnelwall Project</h1>
-        <p className="text-white lead text-center mb-5">
+        <h1 className="text-white mt-5">Welcome to the Tunnelwall Project</h1>
+        <p className="text-white lead mb-4">
         A virtual wall built on the Ethereum blockchain, on which anyone can write to and read from.
         </p>
+        <a 
+          href="https://www.github.com"
+          style={{
+            position: "relative",
+            display: "inline-block"
+          }} >
+          <GithubLogo 
+            className="mb-4"
+            style={{
+              width: "2em",
+              height: "2em",
+              fill: "#fff"
+            }} />
+        </a>
       </Jumbotron>
       <Form
         className="text-center mb-5"
