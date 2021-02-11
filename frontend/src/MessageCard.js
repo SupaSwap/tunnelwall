5801c71a9f92fc905aa7537bec353a78b916a83c
Josh Stow <joshxstow@gmail.com>
2021-02-11 20:01:32 +0000

    update MessageCard component

--- frontend/src/MessageCard.js
+++ frontend/src/MessageCard.js
@@ -6,9 +6,19 @@ export default class MessageCard extends Component {
     return(
       <Card className="w-100">
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Subtitle className="text-muted">ID: { this.props.uid }</Card.Subtitle>
-          <p>{ this.props.text }</p>
+          <Card.Title className="text-center">Successfully did a thing</Card.Title>
+          <hr className="mt-4 mb-3"/>
+          <Card.Subtitle className="mt-3">Post ID</Card.Subtitle>
+          <Card.Text>{ this.props.uid }</Card.Text>
+          <hr className="my-3"/>
+          <Card.Subtitle className="mt-3">Text</Card.Subtitle>
+          <Card.Text>{ this.props.text }</Card.Text>
+          <hr className="my-3"/>
+          <Card.Subtitle className="mt-3">Sender Address</Card.Subtitle>
+          <Card.Text><a href="https://etherscan.io/address/{ this.props.address }">{ this.props.address }</a></Card.Text>
+          <hr className="my-3"/>
+          <Card.Subtitle className="mt-3">Timestamp</Card.Subtitle>
+          <Card.Text>{ this.props.timestamp }</Card.Text>
         </Card.Body>
       </Card>
     )
